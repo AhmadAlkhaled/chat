c9712a8079d860827af7f6a0027822afda9e3eb9
AhmadAlkhaled <almzrab@gmail.com>
2024-02-25 08:05:10 +0100

    changes

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -50,7 +50,7 @@ const ChatPage = () => {
 
   useEffect(() => {
     const token = getCookie('token');
-    const socket = io('https://chat-psi-brown.vercel.app/', {
+    const socket = io('*', {
       query: {
         token: getCookie('token'),
       },
