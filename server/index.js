060a106087b69d1f027713dcae6d9c0495691f00
AhmadAlkhaled <almzrab@gmail.com>
2024-02-25 09:24:32 +0100

    changes

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 
 
 
